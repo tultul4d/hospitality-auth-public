438a35c240a4bf49a7b06a27762af85717fe21c3
tultul <tultulakter963@gmail.com>
2024-04-17 02:21:39 +0600

    update firebase config

--- src/firebase/firebase-config.js
+++ src/firebase/firebase-config.js
@@ -4,14 +4,15 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+console.log('inside firebase config', import.meta.env.VITE_PASS)
 const firebaseConfig = {
-  apiKey: "AIzaSyDEc_nYwRGTW8M-_tnuehE05727PQ-AUJ8",
+  apiKey: import.meta.env.VITE_APIKEY,
   authDomain: "hospitality-auth.firebaseapp.com",
   projectId: "hospitality-auth",
   storageBucket: "hospitality-auth.appspot.com",
   messagingSenderId: "673116789634",
   appId: "1:673116789634:web:ea87b59a71d32071c10d9b"
-};
+}
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
