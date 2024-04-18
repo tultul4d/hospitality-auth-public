53d691a97bb865dc4a462e04980ba840829660a5
tultul <tultulakter963@gmail.com>
2024-04-18 13:36:45 +0600

    added some changes

--- src/pages/UpdateProfile.jsx
+++ src/pages/UpdateProfile.jsx
@@ -1,7 +1,29 @@
+import { useContext } from "react";
 import { Helmet } from "react-helmet-async";
+import { AuthContext } from "../providers/AuthProviders";
+import { updateProfile } from "firebase/auth";
 
 
 const UpdateProfile = () => {
+  const {user} = useContext(AuthContext);
+
+  const handleUpdateProfile = e =>{
+    e.preventDefault();
+    console.log(e.target.name.value);
+    console.log(e.target.photoURL.value);
+    const formData ={
+      displayName: e.target.name.value,
+      photoURL: e.target.photoURL.value
+    }
+
+    updateProfile(user, formData)
+    .then(
+      // window.location.reload()
+    )
+    .catch(error =>{
+      console.log(error);
+    })
+  }
     return (
 
 <div className="hero min-h-screen lg:w-1/2 md:w-3/4 bg-base-200 mx-auto">
@@ -16,24 +38,24 @@ const UpdateProfile = () => {
   
 </div>
 <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-  <form className="card-body">
+  <form onSubmit={handleUpdateProfile} className="card-body">
   <div className="form-control">
       <label className="label">
         <span className="label-text">Name</span>
       </label>
-      <input type="name" name="name" required placeholder="name" className="input input-bordered"  />
+      <input type="name" defaultValue={user?.displayName} name="name" required placeholder="name" className="input input-bordered"  />
     </div>
     <div className="form-control">
       <label className="label">
         <span className="label-text">Photo URL</span>
       </label>
-      <input type="photo" name="photo" required placeholder="photo" className="input input-bordered"  />
+      <input type="photo" defaultValue={user?.photoURL} name="photoURL" required placeholder="photo" className="input input-bordered"  />
     </div>
     <div className="form-control">
       <label className="label">
         <span className="label-text">Email</span>
       </label>
-      <input type="email" name="email" required placeholder="email" className="input input-bordered"  />
+      <input type="email" name="email" disabled required placeholder="email" className="input input-bordered"  />
     </div>
 
     
